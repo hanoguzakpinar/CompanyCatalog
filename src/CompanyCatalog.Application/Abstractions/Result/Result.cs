ac435df4e08097f6a9287a2323394095a892d4ab
Oğuzhan Akpınar <oguz@hanakpinar.com>
2026-05-19 12:50:11 +0300

    result pattern eklendi

---
+++ src/CompanyCatalog.Application/Abstractions/Result/Result.cs
@@ -0,0 +1,41 @@
+namespace CompanyCatalog.Application.Abstractions.Result;
+
+public class Result
+{
+    protected Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+            throw new InvalidOperationException("Başarılı işlem, hata içeremez.");
+        if (!isSuccess && error == Error.None)
+            throw new InvalidOperationException("Başarısız işlem, hata içermelidir.");
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; set; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<T> Success<T>(T value)
+        => new(value, true, Error.None);
+
+    public static Result<T> Failure<T>(Error error)
+        => new(default!, false, error);
+}
+
+public class Result<T> : Result
+{
+    private readonly T _value;
+
+    internal Result(T value, bool isSuccess, Error error) : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    public T Value =>
+        IsSuccess ? _value : throw new InvalidOperationException("Başarısız işlemin değerine erişilemez.");
+}

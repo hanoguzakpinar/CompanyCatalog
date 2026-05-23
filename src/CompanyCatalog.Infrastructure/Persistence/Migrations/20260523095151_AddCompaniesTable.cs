6d5fdd97e7609bf6625e4773fb5df92a55203522
Oğuzhan Akpınar <oguz@hanakpinar.com>
2026-05-23 12:53:59 +0300

    company tablosu için migration

---
+++ src/CompanyCatalog.Infrastructure/Persistence/Migrations/20260523095151_AddCompaniesTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyCatalog.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompaniesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "companies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    tax_number = table.Column<string>(type: "character(10)", fixedLength: true, maxLength: 10, nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_companies_is_active",
+                table: "companies",
+                column: "is_active");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_companies_name",
+                table: "companies",
+                column: "name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_companies_tax_number",
+                table: "companies",
+                column: "tax_number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "companies");
+        }
+    }
+}

b1d371b4c4674b3c8fd8426d568a5badaea3188b
Oğuzhan Akpınar <oguzhan.akpinar@belbim.istanbul>
2026-05-25 16:27:41 +0300

    product domain part 1

---
+++ src/CompanyCatalog.Domain/Products/Product.cs
@@ -0,0 +1,78 @@
+﻿using CompanyCatalog.Domain.Categories;
+using CompanyCatalog.Domain.Common;
+using CompanyCatalog.Domain.Companies;
+
+namespace CompanyCatalog.Domain.Products;
+
+public sealed class Product : AggregateRoot<Guid>
+{
+    private Product()
+    {
+    }
+
+    private Product(
+        Guid id, string name, string? description, string sku, decimal price, int stockQuantity,
+        Guid companyId,
+        Guid categoryId)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Sku = sku;
+        Price = price;
+        StockQuantity = stockQuantity;
+        CompanyId = companyId;
+        CategoryId = categoryId;
+        IsActive = true;
+    }
+
+
+    public string Name { get; private set; } = string.Empty;
+    public string? Description { get; private set; }
+    public string Sku { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+    public int StockQuantity { get; private set; }
+    public bool IsActive { get; set; }
+
+    //Foreign Keys
+    public Guid CompanyId { get; private set; }
+    public Guid CategoryId { get; private set; }
+
+    //Navigation Props
+    public Company? Company { get; set; }
+    public Category? Category { get; set; }
+
+    public static Product Create(string name, string? description, string sku, decimal price, int stockQuantity,
+        Guid companyId, Guid categoryId)
+    {
+        if (string.IsNullOrWhiteSpace(name) || name.Length < 2)
+            throw new DomainException("Ürün ismi en az 2 karakter olmalıdır.");
+        if (name.Length > 200)
+            throw new DomainException("Ürün ismi maksimum 200 karakter olmalıdır.");
+        if (description != null && description.Length > 1000)
+            throw new DomainException("Açıklama maksimum 1000 karakter olmalıdır.");
+        if (string.IsNullOrWhiteSpace(sku) || sku.Length < 3)
+            throw new DomainException("SKU en az 3 karakter olmalıdır.");
+        if (sku.Length > 50)
+            throw new DomainException("SKU maksimum 50 karakter olmalıdır.");
+        if (price < 0)
+            throw new DomainException("Fiyat, negatif olamaz.");
+        if (stockQuantity < 0)
+            throw new DomainException("Stok adeti, negatif olamaz.");
+        if (companyId == Guid.Empty)
+            throw new DomainException("Company belirtilmelidir.");
+        if (categoryId == Guid.Empty)
+            throw new DomainException("Kategori belirtilmelidir.");
+
+        return new Product(
+            Guid.CreateVersion7(),
+            name.Trim(),
+            description?.Trim(),
+            sku.Trim().ToUpperInvariant(),
+            price,
+            stockQuantity,
+            companyId,
+            categoryId
+        );
+    }
+}
